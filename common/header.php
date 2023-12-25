fb7539bbb25e7ce60fefc4a38054f9e12e0b1654
Keerthi <keerthana14298@gmail.com>
2023-12-25 18:52:25 +0530

    Update header page

--- common/header.php
+++ common/header.php
@@ -1,4 +1,4 @@
-<?php error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);?>
+<?php error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING); ?>
 <html lang="en">
 
 <head>
@@ -26,6 +26,36 @@
   <link rel="stylesheet" href="css/package.css" />
   <link rel="stylesheet" href="css/service.css" />
   <link rel="stylesheet" href="css/package-details.css">
+
+  <!-- Search Engine -->
+<meta name="description" content="Best Travel Agent in Coimbatore where you can find domestic & international package. Plan your trip as you wish. A single window to your travel needs.">
+<meta name="image" content="https://aspireholidays.in/asset/images/others/logo.svg">
+<!-- Schema.org for Google -->
+<meta itemprop="name" content="Aspire Holidays">
+<meta itemprop="description" content="Best Travel Agent in Coimbatore where you can find domestic & international package. Plan your trip as you wish. A single window to your travel needs.">
+<meta itemprop="image" content="https://aspireholidays.in/asset/images/others/logo.svg">
+<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+<meta name="og:title" content="Aspire Holidays">
+<meta name="og:description" content="Best Travel Agent in Coimbatore where you can find domestic & international package. Plan your trip as you wish. A single window to your travel needs.">
+<meta name="og:image" content="https://aspireholidays.in/asset/images/others/logo.svg">
+<meta name="og:url" content="https://aspireholidays.in">
+<meta name="og:site_name" content="Aspire Holidays">
+<meta name="fb:admins" content="Aspire Holidays">
+<meta name="og:type" content="website">
+<!---02Feb23 Code for Facebook Open Graph Tag Ends -->
+
+<!-- Code for twitter cards-->
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:site" content="@aspireholidays" />
+<meta name="twitter:title" content="Aspire Holidays" />
+<meta name="twitter:description" content="Best Tour Agent in Coimbatore" />
+<meta name="twitter:image" content="https://aspireholidays.in/asset/images/others/logo.svg" />
+<!-- Code for twitter card ends-->
+
+  <!-- Google search results scripts  -->
+<!--HTML TAG-->
+<meta name="google-site-verification" content="ub_ZBL3e5cugs-F54YwAvByPa4P_rPB6JOwu8SlngXA" />
+<!--HTML TAG END-->
 
   <!-- BreadcrumbList schema  -->
   <script type="application/ld+json">
@@ -72,7 +102,7 @@
   </script>
 
   <!-- Organization scehma  -->
-    <script type="application/ld+json">
+  <script type="application/ld+json">
   {
     "@context": "https://schema.org",
     "@type": "Corporation",
@@ -97,7 +127,7 @@
   </script>
 
   <!-- Local Business  -->
-    <script type="application/ld+json">
+  <script type="application/ld+json">
   {
     "@context": "https://schema.org",
     "@type": "TravelAgency",
@@ -142,132 +172,415 @@
   }
   </script>
 
+  <style>
+    .icons-img:hover {
+      animation: flip 1s;
+    }
+
+    .social1 {
+      margin-right: 20px;
+      display: flex;
+      gap: 5px
+    }
+
+    .social1 div {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      margin-bottom: 5px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: azure;
+      font-size: 15px;
+    }
+
+    .social1 div:hover i {
+      animation-name: social;
+      animation-duration: 1s;
+      animation-iteration-count: infinite;
+    }
+
+    @keyframes social {
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+
+    .s-media1 {
+      background-image: var(--blueG);
+    }
+
+    .s-media2 {
+      background-image: var(--instaG);
+    }
+
+    .s-media3 {
+      background-image: var(--youG);
+    }
+
+    .s-media4 {
+      background-image: var(--whatG);
+    }
+
+    /* Submenu Style  */
+    .sub-menu-box {
+      position: absolute;
+      /* left:160px; */
+      top:0;
+      background-color: #fff;
+      padding: 20px;
+      border-radius: 10px;
+      opacity: 0;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      left: 100%;
+      /* right: 0; */
+    }
+    .sub-menu-box ul{
+      padding-left: 20px;
+    }
+    .sub-menu-box ul li{
+      margin-bottom: 5px;
+    }
+    .sub-menu-box .col-3{
+      width: fit-content;
+    }
+    .sub-menu-box .row{
+      width: fit-content;
+    }
+    .sub-menu-row{
+      flex-wrap: nowrap;
+    }
+    .continetal-bx, .india-bx{
+      transition: all 500ms;
+    }
+    .cbox1:hover .continetal-bx{
+        opacity: 1;
+    }
+    .cbox2:hover .india-bx{
+      opacity: 1;
+    }
+    .india-bx .col-12{
+      width: fit-content;
+      white-space: nowrap;
+    }.cm-ul a{
+      color:black;
+      font-weight: 400;
+    }
+    
+  </style>
+
 
 </head>
 
-<style>
-  .icons-img:hover {
-    animation: flip 1s;
-  }
+<body>
 
-  .social1 {
-    margin-right: 20px;
-    display: flex;
-    gap: 5px
-  }
+  <?php
+  require('config.php');
 
-  .social1 div {
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    margin-bottom: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: azure;
-    font-size: 15px;
-  }
+  // <!-- Get Packages  -->
+  $eastSql = "SELECT DISTINCT country FROM `package` WHERE country IN('Turkey',
+'Israel',
+'jordan',
+'Oman',
+'Egypt',
+'Qatar',
+'Saudi',
+'United Arab Emirates'
+)";
+  $eastStmt = $pdo->prepare($eastSql);
+  $eastStmt->execute();
+  $east = $eastStmt->fetchAll(PDO::FETCH_ASSOC);
 
-  .social1 div:hover i {
-    animation-name: social;
-    animation-duration: 1s;
-    animation-iteration-count: infinite;
-  }
+  $africaSql = "SELECT DISTINCT country FROM `package` WHERE country IN(
+  'Kenya',
+  'Morocco',
+  'Mauritius',
+  'Seyschells', 
+  'zimbave',
+  'Madagascar',
+  'Tanzania',
+  'Southafrica'
+  )";
+  $africaStmt = $pdo->prepare($africaSql);
+  $africaStmt->execute();
+  $africa = $africaStmt->fetchAll(PDO::FETCH_ASSOC);
 
-  @keyframes social {
-    100% {
-      transform: rotate(360deg);
-    }
-  }
+  $americaSql = "SELECT DISTINCT country FROM `package` WHERE country IN(
+    'alaska',
+    'Canada',
+    'USA',
+    'Southamerica'
+    )";
+  $americaStmt = $pdo->prepare($americaSql);
+  $americaStmt->execute();
+  $america = $americaStmt->fetchAll(PDO::FETCH_ASSOC);
 
-  .s-media1 {
-    background-image: var(--blueG);
-  }
+  $asiaSql = "SELECT DISTINCT country FROM `package` WHERE country IN('Thailand', 'malaysia','Singapore','Malaysia',
+    'Singapore',
+    'Bali',
+    'Philipines',
+    'China',
+    'Hongkong',
+    'Indonesia',
+    'Japan',
+    'Taiwan',
+    'kazakhasthan',
+    'southkorea',
+    'uzbekisthan',
+    'vietnam',
+    'combodia',
+    'vietnam',
+    'combodia',
+    'srilanka',
+    'azerbijian',
+    'Maldives',
+    'Mynmar',
+    'bhutan',
+    'nepal',
+    'Georgia',
+    'Armenia',
+    'Mongolia')";
+  $asiaStmt = $pdo->prepare($asiaSql);
+  $asiaStmt->execute();
+  $asia = $asiaStmt->fetchAll(PDO::FETCH_ASSOC);
 
-  .s-media2 {
-    background-image: var(--instaG);
-  }
+  $europeSql = "SELECT DISTINCT country FROM `package` WHERE country IN(
+      'Austria',
+      'Belgium',
+      'Bulgaria',
+      'Crotia',
+      'Czech',
+      'Denmark',
+      'Finland',
+      'France',
+      'Germany',
+      'Greece',
+      'Geenland',
+      'Hungary',
+      'Iceland',
+      'Ireland',
+      'Italy',
+      'Netherland',
+      'Norway',
+      'Portugal',
+      'Romania',
+      'Sweden',
+      'UK',
+      'Spain',
+      'Switzerland'
+      
+      )";
+  $europeStmt = $pdo->prepare($europeSql);
+  $europeStmt->execute();
+  $europe = $europeStmt->fetchAll(PDO::FETCH_ASSOC);
 
-  .s-media3 {
-    background-image: var(--youG);
-  }
+  $pacificSql = "SELECT DISTINCT country FROM `package` WHERE country IN('Australia',
+'Newzealand',
+'Fiji'
+)";
+  $pacificStmt = $pdo->prepare($pacificSql);
+  $pacificStmt->execute();
+  $pacific = $pacificStmt->fetchAll(PDO::FETCH_ASSOC);
 
-  .s-media4 {
-    background-image: var(--whatG);
-  }
-</style>
+  $indiaSql = "SELECT DISTINCT state FROM `package` WHERE state IN(
+  'Tamil Nadu',
+  'Chhattishgarh',
+  'Maharashtra',
+  'Andra Pradesh',
+  'Arunachal Pradesh',
+  'Assam',
+  'Bihar',
+  'Delhi',
+  'Goa',
+  'Gujarat',
+  'Haryana',
+  'Himachal Pradesh',
+  'Jammu & Kashmir',
+  'Jharkhand',
+  'Karnataka',
+  'Kerala',
+  'Madhya Pradesh',
+  'Maharashtra',
+  'Manipur',
+  'Meghalaya',
+  'Mizoram',
+  'Nagaland',
+  'Orissa',
+  'Pondichery',
+  'Punjab',
+  'Rajasthan',
+  'Sikkim',
+  'Uttar Pradesh',
+  'Tripura',
+  'Uttarakhand',
+  'West Bengal')";
+  $indiaStmt = $pdo->prepare($indiaSql);
+  $indiaStmt->execute();
+  $india = $indiaStmt->fetchAll(PDO::FETCH_ASSOC);
+  ?>
+  <!--  section 1-->
+  <section class="main-menu" id="top">
+    <nav class="navbar navbar-expand-lg bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="asset/images/others/logo.svg" alt="aspire_logo" title="aspire_logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
+          aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+          <ul class="navbar-nav mx-auto my-2 my-lg-0 navbar-nav-scroll nav-scr">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Packages
+              </a>
+              <ul class="dropdown-menu">
+                <li class="cbox1"><a class="dropdown-item" href="continental.php">International</a>
+                  <div class="sub-menu-box continetal-bx">
+                    <div class="row sub-menu-row">
+                      <div class="col-3">
+                      <?php if (!empty($europe)) { ?>
+                        <h6>Europe</h6>
+                        <?php } ?>
+                        <ul class="cm-ul">
+                          <?php
+                          foreach ($europe as $row) {
+                            echo '<a href="package.php?country='.$row['country'].'"><li class="cm-li">' . $row['country'] . '</li></a>';
+                          }
+                          ?>
+                        </ul>
+                      </div>
+                      <div class="col-3">
+                      <?php if (!empty($asia)) { ?>
+                        <h6>Asia</h6>
+                        <?php } ?>
+                        <ul class="cm-ul">
+                          <?php
+                          foreach ($asia as $row) {
+                            echo '<a href="package.php?country='.$row['country'].'"><li class="cm-li">' . $row['country'] . '</li></a>';
+                          }
+                          ?>
+                        </ul>
+                      </div>
+                      <div class="col-3">
+                      <?php if (!empty($east)) { ?>
+                        <h6>East</h6>
+                        <?php } ?>
+                        <ul class="cm-ul">
+                          <?php
+                          foreach ($east as $row) {
+                            $uae = $row['country'];
+                            if($row['country'] == 'United Arab Emirates'){
+                              $uae = 'UAE';
+                            }
+                            echo '<a href="package.php?country='.$row['country'].'"><li class="cm-li">' . $uae . '</li></a>';
+                          }
+                          ?>
+                        </ul>
+                        <?php if (!empty($pacific)) { ?>
+                        <h6>Pacific</h6>
+                        <?php } ?>
+                        <ul class="cm-ul">
+                          <?php
+                          foreach ($pacific as $row) {
+                            echo '<a href="package.php?country='.$row['country'].'"><li class="cm-li">' . $row['country'] . '</li></a>';
+                          }
+                          ?>
+                        </ul>
+                      </div>
+                      <div class="col-3">
+                      <?php if (!empty($africa)) { ?>
+                        <h6>Africa</h6>
+                        <?php } ?>
+                        <ul class="cm-ul">
+                          <?php
+                          foreach ($africa as $row) {
+                            echo '<a href="package.php?country='.$row['country'].'"><li class="cm-li">' . $row['country'] . '</li></a>';
+                          }
+                          ?>
+                        </ul>
+                        <?php if (!empty($america)) { ?>
+                        <h6>America</h6>
+                        <?php } ?>
+                        <ul class="cm-ul">
+                          <?php
+                          foreach ($america as $row) {
+                            echo '<a href="package.php?country='.$row['country'].'"><li class="cm-li">' . $row['country'] . '</li></a>';
+                          }
+                          ?>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="cbox2"><a class="dropdown-item" href="india.php">Domestic</a>
+                <div class="sub-menu-box india-bx">
+                    <div class="row sub-menu-row">
+                      <div class="col-12">
+                        <ul class="cm-ul">
+                          <?php
+                          foreach ($india as $row) {
+                            echo '<a href="package.php?state='.$row['state'].'"><li class="cm-li">' . $row['state'] . '</li></a>';
+                          }
+                          ?>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+              </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a href="service.php" class="nav-link">Services</a>
+            </li>
+            <li class="nav-item">
+              <a href="blog.php" class="nav-link">Blogs</a>
+            </li>
+            <li class="nav-item">
+              <a href="contact.php" class="nav-link">Contact</a>
+            </li>
 
-<!--  section 1-->
-<section class="main-menu" id="top">
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="asset/images/others/logo.svg" alt="aspire_logo" title="aspire_logo">
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll"
-        aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarScroll">
-        <ul class="navbar-nav mx-auto my-2 my-lg-0 navbar-nav-scroll nav-scr">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.php">About</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Packages
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="continental.php">International</a></li>
-              <li><a class="dropdown-item" href="india.php">Domestic</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="service.php" class="nav-link">Services</a>
-          </li>
-          <li class="nav-item">
-            <a href="blog.php" class="nav-link">Blogs</a>
-          </li>
-          <li class="nav-item">
-            <a href="contact.php" class="nav-link">Contact</a>
-          </li>
-
-        </ul>
-        <div>
+          </ul>
+          <div>
 
 
-          <div class="social1">
-            <a class="sm1" href="https://facebook.com/aspireholidays">
-              <div class="s-media1"><i class="fa-brands fa-facebook-f"></i></div>
-            </a>
-            <a class="sm1" href="https://www.instagram.com/aspireholidays.in/">
-              <div class="s-media2"><i class="fa-brands fa-instagram"></i></div>
-            </a>
-            <a class="sm1" href="https://www.youtube.com/@AspireHolidays">
-              <div class="s-media3"><i class="fa-brands fa-youtube"></i></div>
-            </a>
-            <a class=" sm1" href="https://wa.me/+919362266666">
-              <div class="s-media4"><i class="fa-brands fa-whatsapp"></i></div>
-            </a>
+            <div class="social1">
+              <a class="sm1" href="https://facebook.com/aspireholidays">
+                <div class="s-media1"><i class="fa-brands fa-facebook-f"></i></div>
+              </a>
+              <a class="sm1" href="https://www.instagram.com/aspireholidays.in/">
+                <div class="s-media2"><i class="fa-brands fa-instagram"></i></div>
+              </a>
+              <a class="sm1" href="https://www.youtube.com/@AspireHolidays">
+                <div class="s-media3"><i class="fa-brands fa-youtube"></i></div>
+              </a>
+              <a class=" sm1" href="https://wa.me/+919362266666">
+                <div class="s-media4"><i class="fa-brands fa-whatsapp"></i></div>
+              </a>
+            </div>
+
           </div>
-
         </div>
       </div>
-    </div>
-  </nav>
-</section>
-<!--  section 1 end -->
+    </nav>
+  </section>
+  <!--  section 1 end -->
 
-<script>
-  $(document).ready(function () {
-    $("ul >li.navbar-nav").click(function (e) {
-      $("ul >li.navbar-nav").removeClass("active");
-      $(this).addClass("active");
+
+
+  <script>
+    $(document).ready(function () {
+      $("ul >li.navbar-nav").click(function (e) {
+        $("ul >li.navbar-nav").removeClass("active");
+        $(this).addClass("active");
+      });
     });
-  });
-</script>
-
-<body>
+  </script>
