43ffaff7398febfcabd91afc98a4e6c175843b66
Keerthi <keerthana14298@gmail.com>
2024-01-03 16:44:31 +0530

    Status Added

--- admin/add_visa_package.php
+++ admin/add_visa_package.php
@@ -111,6 +111,13 @@ include('common/header.php');
                     <label for="">Fees <span class='text-danger'>*</span></label>
                     <input type="text" name="fees" class="form-control" placeholder="Validity" required>
                 </div>
+                <div class="col">
+                    <label for="">Status <span class='text-danger'>*</span></label>
+                    <select required name="status" id="status" class="form-control">
+                        <option value="1">Active</option>
+                        <option value="0">Inactive</option>
+                    </select>
+                </div>
             </div>
             
 
